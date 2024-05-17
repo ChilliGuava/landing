3653e3f209ff0429fd96b2a2100db5f0991b56dd
Rushat Gabhane <rushatgabhane@gmail.com>
2024-05-18 02:11:14 +0530

    add padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 export default function Home() {
   return (
     <main className="flex h-screen">
-      <div className="m-auto p-6">
+      <div className="m-auto p-4">
         <h1 className="font-bold md:text-6xl text-6xl">
           Build better software
         </h1>
         <h1 className="font-bold md:text-6xl text-6xl">faster ⚡</h1>
-        <p className="md:text-2xl text-base md:mt-5 mt-7">
+        <p className="md:text-2xl text-base md:mt-5 mt-6">
           Outsource your company&#39;s software development effort to engineers
           with experience from Google, Microsoft and Amazon.
         </p>
