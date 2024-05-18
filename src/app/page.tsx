1b7eafedff5b6b33c69be67f1381acd7f825474d
Rushat Gabhane <rushatgabhane@gmail.com>
2024-05-18 12:09:20 +0530

    text sz

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex h-screen">
       <div className="m-auto p-4 max-w-85">
-        <h1 className="font-bold md:text-6xl text-5xl">
+        <h1 className="font-bold md:text-6xl text-4xl">
           Build better software <span className="block">faster ⚡</span>
         </h1>
         <p className="md:text-2xl text-base md:mt-5 mt-6">
